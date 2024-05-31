471121bb7023848cc873dd9492dcd4de3fabaa43
Steve Wamsley <swamsley@protonmail.com>
2024-05-31 09:44:08 -0500

    progress bar improvements

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
-import { toPercent, toRatio, withinRange } from '../common/conversions';
+import { toPercent, withinRange } from '../common/conversions';
 import { CurrencyPipe } from '../pipes/currency.pipe';
 import { ProfitLossText } from '../profit-loss-text/profit-loss-text.component';
 
@@ -29,15 +29,55 @@ export class ProgressBar implements OnChanges {
 
   faCaretDown = faCaretDown;
 
+  lowerBoundLabel = this.lowerLimitLabel;
+  upperBoundLabel = this.upperLimitLabel;
+
+  lowerBound = this.lowerLimit;
+  upperBound = this.upperLimit;
+
   progress: number | null = null;
 
-  progressClass = {
-    'one-way': true,
-    'two-way': false,
+  slLabelStyle = {
+    left: '0%'
   }
 
-  currentIndicatorStyle = {
+  slLabelClass = {
+    ['left-align']: false,
+    ['right-align']: false
+  }
+
+  opLabelStyle = {
     left: '0%'
+  }
+
+  opLabelClass = {
+    ['left-align']: false,
+    ['right-align']: false
+  }
+
+  tpLabelStyle = {
+    left: '0%'
+  }
+
+  tpLabelClass = {
+    ['left-align']: false,
+    ['right-align']: false
+  }
+
+  slIndicatorStyle = {
+    left: '0%'
+  }
+
+  cpIndicatorStyle = {
+    left: '0%'
+  }
+
+  opIndicatorStyle = {
+    left: '50%'
+  }
+
+  tpIndicatorStyle = {
+    left: '50%'
   }
 
   stopLossProgressStyle = {
@@ -53,7 +93,10 @@ export class ProgressBar implements OnChanges {
   };
 
   ngOnChanges() {
-    this.currentIndicatorStyle.left = '0%';
+    this.slIndicatorStyle.left = '0%';
+    this.opIndicatorStyle.left = '0%';
+    this.tpIndicatorStyle.left = '0%';
+    this.cpIndicatorStyle.left = '0%';
     this.losingProgressStyle.width = '0%';
     this.winningProgressStyle.width = '0%';
 
@@ -64,15 +107,26 @@ export class ProgressBar implements OnChanges {
     const op = this.startingValue;
     const tp = this.upperLimit;
     const sl = this.lowerLimit != null ? this.lowerLimit : op;
+
+    this.lowerBoundLabel = this.lowerLimit != null ? this.lowerLimitLabel || 'SL' : 'OP';
+    this.upperBoundLabel = this.upperLimitLabel || 'TP';
+
     const cp = this.currentValue;
     const pl = cp - op;
-    const totalRange = tp - sl;
 
-    const cpPercent = withinRange(toPercent(cp - sl, totalRange), 0, 100);
+    this.lowerBound = Math.min(cp, sl);
+    this.upperBound = Math.max(cp, tp);
+    const totalRange = this.upperBound - this.lowerBound;
+
+    const slIndicatorPercent = withinRange(toPercent(sl - this.lowerBound, totalRange), 0, 100);
+    const opIndicatorPercent = withinRange(toPercent(op - this.lowerBound, totalRange), 0, 100);
+    const tpIndicatorPercent = withinRange(toPercent(tp - this.lowerBound, totalRange), 0, 100);
+    const cpIndicatorPercent = withinRange(toPercent(cp - this.lowerBound, totalRange), 0, 100);
 
     let slPercent = 0;
     let lPercent = 0;
     let pPercent = 0;
+    this.progress = null;
 
     if (pl < 0) {
       slPercent = withinRange(toPercent(op - sl + pl, totalRange), 0, 100);
@@ -82,9 +136,35 @@ export class ProgressBar implements OnChanges {
       pPercent = withinRange(toPercent(pl, totalRange), 0, 100);
     }
 
-    this.currentIndicatorStyle.left = `${cpPercent}%`;
+    this.slLabelStyle.left = `${slIndicatorPercent}%`;
+    this.setLabelClass(this.slLabelClass, slIndicatorPercent);
+    this.opLabelStyle.left = `${opIndicatorPercent}%`;
+    this.setLabelClass(this.opLabelClass, opIndicatorPercent);
+    this.tpLabelStyle.left = `${tpIndicatorPercent}%`;
+    this.setLabelClass(this.tpLabelClass, tpIndicatorPercent);
+
+    this.cpIndicatorStyle.left = `${cpIndicatorPercent}%`;
+    this.slIndicatorStyle.left = `${slIndicatorPercent}%`;
+    this.opIndicatorStyle.left = `${opIndicatorPercent}%`;
+    this.tpIndicatorStyle.left = `${tpIndicatorPercent}%`;
+
     this.stopLossProgressStyle.width = `${slPercent}%`;
     this.losingProgressStyle.width = `${lPercent}%`;
     this.winningProgressStyle.width = `${pPercent}%`;
+
+    this.progress = (cp - op) / (tp - op) * 100;
+  }
+
+  setLabelClass(labelClass: any, leftPercent: number) {
+    if (leftPercent < 10) {
+      labelClass['left-align'] = true;
+      labelClass['right-align'] = false;
+    } else if (leftPercent > 90) {
+      labelClass['left-align'] = false;
+      labelClass['right-align'] = true;
+    } else {
+      labelClass['left-align'] = false;
+      labelClass['right-align-right'] = false;
+    }
   }
 }
